dfc18ad050695da641ef265e63fe8135e4093ee5
ma1de <ilyabuhtoyarov@protonmail.ch>
2023-10-19 09:10:57 +0500

    makefile implementation, src folder

---
+++ src/main.cpp
@@ -0,0 +1,222 @@
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ctype.h>
+
+enum class TokenType {
+    FORWARD,
+    BACKWARD,
+    FORWARD_HALF,
+    BACKWARD_HALF,
+    SQUARE,
+    SQUARE_ROOT,
+    DIGIT,
+    SPACE
+};
+
+typedef struct {
+    TokenType type;
+    int value;
+} Token;
+
+struct SyntaxToken {
+    TokenType type;
+    char value;
+
+    SyntaxToken(){};
+
+    SyntaxToken(TokenType init_type, char init_value) {
+        type = init_type;
+        value = init_value;
+    };
+};
+
+vector<SyntaxToken> syntaxTokens = {
+    SyntaxToken(TokenType::FORWARD, '>'),
+    SyntaxToken(TokenType::BACKWARD, '<'),
+    SyntaxToken(TokenType::FORWARD_HALF, ';'),
+    SyntaxToken(TokenType::BACKWARD_HALF, ':'),
+    SyntaxToken(TokenType::SQUARE, '^'),
+    SyntaxToken(TokenType::SQUARE_ROOT, '['),
+    SyntaxToken(TokenType::SPACE, '|')
+};
+
+vector<Token> tokenize(string code) {
+    vector<Token> tokens;
+
+    for (int i = 0; i < code.length(); i++) {
+        Token token;
+        bool noneMatch;
+
+        for (SyntaxToken syntaxToken: syntaxTokens) {
+            if (code.at(i) != syntaxToken.value) {
+                noneMatch = true;
+                continue;
+            }
+
+            token.type = syntaxToken.type;
+
+            tokens.push_back(token);
+            noneMatch = false;
+            break;
+        }
+
+        if (isdigit(code.at(i))) {
+            token.type = TokenType::DIGIT;
+            token.value = code.at(i) - '0';
+
+            tokens.push_back(token);
+            continue;
+        }
+
+        if (noneMatch) {
+            cerr << "UNEXPECTED SYMBOL AT " << code.at(i) << " SHUTTING DOWN" << endl;
+            exit(404);
+        }
+    }
+
+    return tokens;
+}
+
+int count(vector<Token> tokens, TokenType type) {
+    switch (type) {
+        case TokenType::FORWARD:
+            return count_if(tokens.begin(), tokens.end(), [](Token a) { return a.type == TokenType::FORWARD; });
+        case TokenType::BACKWARD:
+            return count_if(tokens.begin(), tokens.end(), [](Token a) { return a.type == TokenType::BACKWARD; });
+        case TokenType::FORWARD_HALF:
+            return count_if(tokens.begin(), tokens.end(), [](Token a) { return a.type == TokenType::FORWARD_HALF; });
+        case TokenType::BACKWARD_HALF:
+            return count_if(tokens.begin(), tokens.end(), [](Token a) { return a.type == TokenType::BACKWARD_HALF; });
+        case TokenType::SQUARE:
+            return count_if(tokens.begin(), tokens.end(), [](Token a) { return a.type == TokenType::SQUARE; });
+        case TokenType::SQUARE_ROOT:
+            return count_if(tokens.begin(), tokens.end(), [](Token a) { return a.type == TokenType::SQUARE_ROOT; });
+        default:
+            return 0;
+    }
+} 
+
+vector<string> getAllSpaces(string contents) {
+    vector<string> spliced;
+    string delimiter = "|";
+    int pos = 0;
+    string token;
+
+    while ((pos = contents.find(delimiter)) != string::npos) {
+        token = contents.substr(0, pos);
+        spliced.push_back(token);
+        contents.erase(0, pos + delimiter.length());
+    }
+
+    spliced.push_back(contents);
+
+    vector<string> vectors;
+
+    for (string content : spliced) {
+        vectors.push_back(content);
+    }
+
+    return vectors;
+}
+
+vector<int> getAllDigits(vector<Token> tokens) {
+    vector<int> digits;
+
+    for (Token token : tokens) {
+        if (token.type != TokenType::DIGIT) {
+            continue;
+        }
+
+        digits.push_back(token.value);
+    }
+
+    return digits;
+}
+
+double tokens_to_output(vector<Token> tokens) {
+    double output = 0;
+
+    int forward = count(tokens, TokenType::FORWARD);
+    int backward = count(tokens, TokenType::BACKWARD);
+    double forwardHalf = count(tokens, TokenType::FORWARD_HALF) / 2.0;
+    double backwardHalf = count(tokens, TokenType::BACKWARD_HALF) / 2.0;
+    int square = count(tokens, TokenType::SQUARE);
+    int squareRoot = count(tokens, TokenType::SQUARE_ROOT);
+
+    output = (forward + forwardHalf) - (backward + backwardHalf);
+
+    if (square > 0) {
+        output = pow(output, square);
+    }
+
+    if (squareRoot > 0) {
+        output = sqrt(output);
+    }
+
+    if (getAllDigits(tokens).size() > 0) {
+        vector<int> digits = getAllDigits(tokens);
+
+        for (int i : digits) {
+            output += i;
+        }
+    }
+
+    return output;
+}
+
+bool hasSuffix(string a, string suffix) {
+    return a.size() >= suffix.size() && a.compare(a.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+int main(int argc, char * argv[]) {
+    string fileName = "code.abd";
+
+    if (argv[1] != nullptr) {
+        fileName = argv[1];
+    }
+
+    if (!hasSuffix(fileName, ".abd")) {
+        cerr << "Only files that have the `.abd` extension are allowed." << endl;
+        exit(404);
+    }
+
+    string contents;
+    ifstream inputStream("code.abd");
+
+    if (!inputStream) {
+        cerr << "Couldn't open the code.abd file." << endl;
+        exit(404);
+    }
+
+    string line;
+
+    while (getline(inputStream, line)) {
+        contents += line;
+    }
+
+    vector<string> spaces = getAllSpaces(contents);
+
+    if (spaces.size() >= 2) {
+        int vectorCount = 0;
+
+        for (string str : spaces) {
+            vectorCount++;
+
+            cout << "(Part " << vectorCount << ") Output (char): " << (char) tokens_to_output(tokenize(str)) << endl;
+            cout << "(Part " << vectorCount << ") Output (int): " << tokens_to_output(tokenize(str)) << endl;
+        }
+
+        return 0;
+    }
+
+    const vector<Token> result = tokenize(contents);
+
+    cout << "Output (char): " << (char) tokens_to_output(result) << endl;
+    cout << "Output (int): " << tokens_to_output(result) << endl;
+
+    return 0;
+}
